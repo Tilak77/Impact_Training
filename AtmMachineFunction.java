d7f2cd09473ec294a417c0744340adec8fcdee85
Tilak77 <sparatane1211@gmail.com>
2025-05-15 08:40:14 +0530

    message

---
+++ AtmMachineFunction.java
@@ -0,0 +1,19 @@
+public class AtmMachineFunction {
+    
+    static int check_balance(int n){
+        
+        return n;
+    }
+
+    static int withdraw(int n){
+        int withdraw_m= -n;
+    }
+    
+    
+    public static void main(String[] args) {
+        int amount=200000;
+        int w;
+        check_balance(amount);
+        withdraw(w);
+    }
+}
