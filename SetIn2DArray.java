0ffa69aadc44af22702fbbbe65ee14f2695fdc10
Tilak77 <sparatane1211@gmail.com>
2025-05-21 00:24:36 +0530

    message

--- SetIn2DArray.java
+++ SetIn2DArray.java
@@ -8,7 +8,8 @@ public class SetIn2DArray{
         int col=sc.nextInt();
         int[][] arr1= new int[row][col];
 
-        
+        int count1=0;
+        int count2=0;
 
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
@@ -22,10 +23,19 @@ public class SetIn2DArray{
 
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){            
-            if(x==arr1[i][j]){
-                System.out.print("(" + i + ", " + j + ")");
+            // if(x==arr1[i][j]){
+            //     System.out.print("(" + i + ", " + j + ")");
+            // }
+            
+            //Check if Element is Diagonal or Not--------------
+            
+            count1=count1+1;
+            count2=count2+1;
+
+            if(x==count1 || x==count2 ){
+                System.out.println("Element is Diagonal");
             }
-        }
+            }
         }
     
     sc.close();
